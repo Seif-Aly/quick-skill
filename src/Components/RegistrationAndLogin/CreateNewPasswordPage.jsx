0feb25e250edb90b0bb2b8feff02b3f214b3d109
seif <saly@edu.hse.ru>
2024-03-30 11:39:03 +0300

    update newPass page

--- src/Components/RegistrationAndLogin/CreateNewPasswordPage.jsx
+++ src/Components/RegistrationAndLogin/CreateNewPasswordPage.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
 import NavbarSignedOut from "../Navbars/NavbarSignedOut";
+import { useNavigate } from "react-router-dom";
 import "../Style/RegistrationAndLogin.css";
 
 const CreateNewPasswordPage = () => {
@@ -9,6 +10,8 @@ const CreateNewPasswordPage = () => {
   const [confirmPassword, setConfirmPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
   const [showPassword2, setShowPassword2] = useState(false);
+  const [passwordChanged, setPasswordChanged] = useState(false);
+  const navigate = useNavigate();
 
   const toggleShowPassword = () => {
     setShowPassword(!showPassword);
@@ -20,13 +23,21 @@ const CreateNewPasswordPage = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    if (newPassword && newPassword === confirmPassword) {
-      // Logic to update the password
+    if (
+      newPassword &&
+      newPassword === confirmPassword &&
+      newPassword &&
+      newPassword >= 6
+    ) {
       console.log("Password updated");
+      setPasswordChanged(true);
     } else {
-      // Show some error to the user
       console.log("Passwords do not match");
     }
+  };
+
+  const goToLogin = () => {
+    navigate("/login");
   };
 
   return (
@@ -34,53 +45,66 @@ const CreateNewPasswordPage = () => {
       <NavbarSignedOut />
       <Container>
         <Row className="justify-content-center">
-          <Col md={6} className="reset-password-form">
-            <h2>Create new password 🔑</h2>
-            <Form onSubmit={handleSubmit}>
-              <Form.Group
-                controlId="formNewPassword"
-                className="form-group-custom"
-              >
-                <div className="password-toggle">
-                  <Form.Control
-                    type={showPassword ? "text" : "password"}
-                    placeholder="New password"
-                    value={newPassword}
-                    onChange={(e) => setNewPassword(e.target.value)}
-                  />
-                  <Button
-                    onClick={toggleShowPassword}
-                    className="toggle-password-btn"
-                  >
-                    {showPassword ? <FaEyeSlash /> : <FaEye />}
-                  </Button>
-                </div>
-              </Form.Group>
-              <Form.Group
-                controlId="formConfirmPassword"
-                className="form-group-custom"
-              >
-                <div className="password-toggle">
-                  <Form.Control
-                    type={showPassword2 ? "text" : "password"}
-                    placeholder="Again new password"
-                    value={confirmPassword}
-                    onChange={(e) => setConfirmPassword(e.target.value)}
-                  />
-                  <Button
-                    onClick={toggleShowPassword2}
-                    className="toggle-password-btn"
-                  >
-                    {showPassword2 ? <FaEyeSlash /> : <FaEye />}
-                  </Button>
-                </div>
-              </Form.Group>
-
-              <Button variant="primary" type="submit" className="mt-4">
-                Reset password
+          {passwordChanged ? (
+            <Col md={6} className="reset-password-form">
+              <h2>Create new password 🔑</h2>
+              <p>
+                Congratulations 🎉 Your password was successfully changed. Now
+                you can login with new password.
+              </p>
+              <Button onClick={goToLogin} className="mt-4" variant="primary">
+                Go to QuickSkill
               </Button>
-            </Form>
-          </Col>
+            </Col>
+          ) : (
+            <Col md={6} className="reset-password-form">
+              <h2>Create new password 🔑</h2>
+              <Form onSubmit={handleSubmit}>
+                <Form.Group
+                  controlId="formNewPassword"
+                  className="form-group-custom"
+                >
+                  <div className="password-toggle">
+                    <Form.Control
+                      type={showPassword ? "text" : "password"}
+                      placeholder="New password"
+                      value={newPassword}
+                      onChange={(e) => setNewPassword(e.target.value)}
+                    />
+                    <Button
+                      onClick={toggleShowPassword}
+                      className="toggle-password-btn"
+                    >
+                      {showPassword ? <FaEyeSlash /> : <FaEye />}
+                    </Button>
+                  </div>
+                </Form.Group>
+                <Form.Group
+                  controlId="formConfirmPassword"
+                  className="form-group-custom"
+                >
+                  <div className="password-toggle">
+                    <Form.Control
+                      type={showPassword2 ? "text" : "password"}
+                      placeholder="Again new password"
+                      value={confirmPassword}
+                      onChange={(e) => setConfirmPassword(e.target.value)}
+                    />
+                    <Button
+                      onClick={toggleShowPassword2}
+                      className="toggle-password-btn"
+                    >
+                      {showPassword2 ? <FaEyeSlash /> : <FaEye />}
+                    </Button>
+                  </div>
+                </Form.Group>
+
+                <Button variant="primary" type="submit" className="mt-4">
+                  Reset password
+                </Button>
+              </Form>
+            </Col>
+          )}
         </Row>
       </Container>
     </>
